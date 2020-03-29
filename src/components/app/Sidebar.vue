0083422a77cf68519ed83a5b649c00b9b49a8798
Nazar Lesyuk <pronaza@gmail.com>
2020-03-29 22:19:32 +0300

    create sidebar, navbar. sidebar is DONE

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul class="sidenav app-sidenav open">
+      <router-link
+        v-for="link in links"
+        :key="link.url"
+        tag="li"
+        active-class="active"
+        :to="link.url"
+        :exact="link.exact"
+      >
+        <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+      </router-link>
+    </ul>
+</template>
+<script>
+export default {
+  data: () => ({
+    links: [
+      {title: 'Счет', url: '/', exact: true},
+      {title: 'История', url: 'history'},
+      {title: 'Планирование', url: 'planing'},
+      {title: 'Новая запись', url: 'record'},
+      {title: 'Категории', url: 'categories'},
+    ]
+  })
+}
+</script>
