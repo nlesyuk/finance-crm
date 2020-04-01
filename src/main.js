f427a4ea2ac35e4a8fcffe5d8984a6ac40ec3e0c
Nazar Lesyuk <pronaza@gmail.com>
2020-04-01 23:17:22 +0300

    firebase: install, init

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import messagePlugin from '@/utils/message.plugin'
 import './registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min'; // from node_modules
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
 Vue.config.productionTip = false;
 
@@ -16,8 +19,29 @@ Vue.filter('dateFilter', dateFilter)
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJWjn5rgRsfM6Fhjz4wJnBRtMZfjol1t8U",
+  authDomain: "vue-fincrm-173a0.firebaseapp.com",
+  databaseURL: "https://vue-fincrm-173a0.firebaseio.com",
+  projectId: "vue-fincrm-173a0",
+  storageBucket: "vue-fincrm-173a0.appspot.com",
+  messagingSenderId: "253469932060",
+  appId: "1:253469932060:web:17876bdfeb4fce7fb5bdd9",
+  measurementId: "G-V9R237T0Y4"
+};
+firebase.initializeApp(firebaseConfig)
+
+// firebase.auth().onAuthStateChanged(calback) ищет локальные данные пользователя для автоматической авторизации
+// вызывается onAuthStateChanged нескольо раз во время сесии юзера (авториз, выход) поэтому нужно делать так
+let app
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount('#app');
+  }
+})
+
