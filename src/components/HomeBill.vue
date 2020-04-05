0883347b2345ccec79ea6b3b8c3f8bcdb590a761
Nazar Lesyuk <pronaza@gmail.com>
2020-04-05 11:57:17 +0300

    from Home create 2 child components

---
+++ src/components/HomeBill.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="col s12 m6 l4">
+    <div class="card light-blue bill-card">
+      <div class="card-content white-text">
+        <span class="card-title">Счет в валюте</span>
+
+        <p class="currency-line">
+          <span>12.0 Р</span>
+        </p>
+        <p class="currency-line">
+          <span>12.0 $</span>
+        </p>
+        <p class="currency-line">
+          <span>12.0 ~</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
