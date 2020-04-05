635fbd029b5b76c88c876dd4034afae7e832a987
Nazar Lesyuk <pronaza@gmail.com>
2020-04-05 16:40:45 +0300

    refresh button - DONE

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,10 @@
     <div class="page-title">
       <h3>Счет</h3>
 
-      <button class="btn waves-effect waves-light btn-small">
+      <button
+        class="btn waves-effect waves-light btn-small"
+        @click="refresh"
+      >
         <i class="material-icons">refresh</i>
       </button>
     </div>
@@ -35,13 +38,19 @@ export default {
     loading: true,
     currency: null
   }),
+  methods: {
+    async refresh() {
+      this.loading = true
+      this.currency = await this.$store.dispatch('fetchCurrency')
+      this.loading = false
+    }
+  },
   components: {
     HomeBill,
     HomeCurrency
   },
   async mounted(){
     this.currency = await this.$store.dispatch('fetchCurrency')
-    console.log(this.currency)
     this.loading = false
   }
 };
