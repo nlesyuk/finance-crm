a3481fb160d4837bdcfe74b36ad541d6299f3b5b
Nazar Lesyuk <pronaza@gmail.com>
2020-04-05 17:46:09 +0300

    prepera components for categories

---
+++ src/components/CategoryCreate.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="col s12 m6">
+    <div>
+      <div class="page-subtitle">
+        <h4>Создать</h4>
+      </div>
+
+      <form>
+        <div class="input-field">
+          <input
+              id="name"
+              type="text"
+          >
+          <label for="name">Название</label>
+          <span class="helper-text invalid">Введите название</span>
+        </div>
+
+        <div class="input-field">
+          <input
+              id="limit"
+              type="number"
+          >
+          <label for="limit">Лимит</label>
+          <span class="helper-text invalid">Минимальная величина</span>
+        </div>
+
+        <button class="btn waves-effect waves-light" type="submit">
+          Создать
+          <i class="material-icons right">send</i>
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'category create'
+}
+</script>
