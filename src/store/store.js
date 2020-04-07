38025480bee83e98627d67da7b687a20230b46ae
Nazar Lesyuk <pronaza@gmail.com>
2020-04-08 00:42:58 +0300

    Record: BE logic - DONE

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex';
 import auth from './auth'
 import info from './info'
 import category from './category'
+import record from './record'
 
 Vue.use(Vuex);
 
@@ -33,5 +34,6 @@ export default new Vuex.Store({
     auth,
     info,
     category,
+    record
   },
 });
--- src/views/Record.vue
+++ src/views/Record.vue
@@ -87,7 +87,7 @@
 
 <script>
 import {required, minValue} from 'vuelidate/lib/validators'
-
+import {mapGetters} from 'vuex'
 export default {
   name: 'record',
   data: () => ({
@@ -103,11 +103,47 @@ export default {
     amount: { minValue: minValue(1) },
     description: { required },
   },
+  computed: {
+    ...mapGetters(['info']),
+    canCreateRecord() {
+      if (this.type === 'income') {
+        return true
+      }
+      return this.info.bill >= this.amount
+    }
+  },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if(this.$v.$invalid) {
         this.$v.$touch()
+        console.log(this.$v.amount)
         return
+      }
+
+      if(this.canCreateRecord) {
+        try {
+          await this.$store.dispatch('createRecord', {
+            categoryId: this.category,
+            amount: this.amount,
+            description: this.description,
+            type: this.type,
+            date: new Date().toJSON(),
+          })
+
+          const bill = this.type === 'income'
+            ? this.info.bill + this.amount
+            : this.info.bill - this.amount
+
+          await this.$store.dispatch('updateInfo', {bill})
+          this.$message('Запись успешно создана')
+          this.$v.$reset()
+          this.amount = 1
+          this.description = ''
+        } catch (e) {
+          // catch error in vuex
+        }
+      } else {
+        this.$message(`Недостаточно денег ${this.amount - this.info.bill}`)
       }
 
     }
