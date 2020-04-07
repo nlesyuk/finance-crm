509fb239656bc5219f31dca8655fd95b1231cbc0
Nazar Lesyuk <pronaza@gmail.com>
2020-04-07 23:49:26 +0300

    Record: FE logic - DONE, BE logic - in progress

--- src/store/info.js
+++ src/store/info.js
@@ -18,7 +18,6 @@ export default {
         const uid = await dispatch('getUid')
         const info = (await firebase.database().ref(`/users/${uid}/info`).once('value')).val()
         commit('setInfo', info)
-        console.log(uid)
       } catch(e) {
         throw new Error(e)
       }
--- src/views/Record.vue
+++ src/views/Record.vue
@@ -4,11 +4,21 @@
       <h3>Новая запись</h3>
     </div>
 
-    <form class="form">
+    <Loader v-if="loading" />
+
+    <p class="center" v-else-if="!categories.length">
+      Категорий пока нет.
+      <router-link to="categories">Добавить новую категорию</router-link>
+    </p>
+
+    <form class="form" v-else @submit.prevent="submitHandler">
       <div class="input-field" >
-        <select>
+        <select ref="select" v-model="category">
           <option
-          >name cat</option>
+            v-for="cat in categories"
+            :key="cat.id"
+            :value="cat.id"
+          >{{ cat.title }}</option>
         </select>
         <label>Выберите категорию</label>
       </div>
@@ -20,6 +30,7 @@
               name="type"
               type="radio"
               value="income"
+              v-model="type"
           />
           <span>Доход</span>
         </label>
@@ -32,6 +43,7 @@
               name="type"
               type="radio"
               value="outcome"
+              v-model="type"
           />
           <span>Расход</span>
         </label>
@@ -41,19 +53,28 @@
         <input
             id="amount"
             type="number"
+            v-model.number="amount"
+            :class="{invalid: $v.amount.$dirty && !$v.amount.minValue}"
         >
         <label for="amount">Сумма</label>
-        <span class="helper-text invalid">amount пароль</span>
+        <span
+          class="helper-text"
+          v-if="$v.amount.$dirty && !$v.amount.minValue"
+        >Минимальная величина {{ $v.amount.$params.minValue.min }}</span>
       </div>
 
       <div class="input-field">
         <input
             id="description"
             type="text"
+            v-model="description"
+            :class="{invalid: $v.description.$dirty && !$v.description.required}"
         >
         <label for="description">Описание</label>
         <span
-              class="helper-text invalid">description пароль</span>
+          class="helper-text"
+          v-if="$v.description.$dirty && !$v.description.required"
+        >Введите описание</span>
       </div>
 
       <button class="btn waves-effect waves-light" type="submit">
@@ -63,3 +84,50 @@
     </form>
   </div>
 </template>
+
+<script>
+import {required, minValue} from 'vuelidate/lib/validators'
+
+export default {
+  name: 'record',
+  data: () => ({
+    loading: true,
+    select: null,
+    categories: [],
+    category: null,
+    type: 'outcome',
+    amount: 1,
+    description: '',
+  }),
+  validations: {
+    amount: { minValue: minValue(1) },
+    description: { required },
+  },
+  methods: {
+    submitHandler() {
+      if(this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+
+    }
+  },
+  async mounted() {
+    this.categories = await this.$store.dispatch('fetchCategories')
+    this.loading = false
+    if(this.categories.length) {
+      this.category = this.categories[0].id
+    }
+
+    setTimeout(() => {
+      this.select = M.FormSelect.init(this.$refs.select)
+      M.updateTextFields()
+    }, 0)
+  },
+  beforeDestroy() {
+    if ( this.select && this.select.destroy ) {
+      this.select.destroy()
+    }
+  }
+}
+</script>
