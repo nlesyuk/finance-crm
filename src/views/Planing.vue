c194a2277d1c7f23e431ad7096941de0a18038bd
Nazar Lesyuk <pronaza@gmail.com>
2020-04-09 23:56:13 +0300

    Planing: base functional - DONE

--- src/views/Planing.vue
+++ src/views/Planing.vue
@@ -2,22 +2,78 @@
   <div>
     <div class="page-title">
       <h3>Планирование</h3>
-      <h4>12 212</h4>
+      <h4>{{ info.bill | currencyFilter()}}</h4>
     </div>
 
-    <section>
-      <div>
+    <Loader v-if="loading" />
+
+    <p class="center" v-else-if="!categories.length">
+      Категорий пока нет.
+      <router-link to="categories">Добавить новую категорию</router-link>
+    </p>
+
+    <section v-else>
+      <div v-for="cat of categories" :key="cat.id">
         <p>
-          <strong>Девушка:</strong>
-          12 122 из 14 0000
+          <strong>{{ cat.title }}</strong>
+          {{ cat.spend | currencyFilter() }} из {{ cat.limit | currencyFilter() }}
         </p>
         <div class="progress" >
           <div
-              class="determinate green"
-              style="width:40%"
+              class="determinate"
+              :class="[cat.progreeColor]"
+              :style="{width: cat.progreePercent + '%'}"
           ></div>
         </div>
       </div>
     </section>
+
   </div>
 </template>
+
+<script>
+import {mapGetters} from 'vuex'
+
+export default {
+  name: 'planing',
+  data: () => ({
+    loading: true,
+    categories: [],
+  }),
+  computed: {
+    ...mapGetters(['info'])
+  },
+  async mounted() {
+    const records = await this.$store.dispatch('fetchRecords')
+    const categories = await this.$store.dispatch('fetchCategories')
+
+    console.log(records)
+    console.log(categories)
+
+    this.categories = categories.map(cat => {
+      const spend = records
+        .filter(c => c.categoryId === cat.id)
+        .filter(c => c.type === 'outcome')
+        .reduce((total, record) => {
+          return total + +record.amount
+        }, 0)
+
+        const percent = 100 * spend / cat.limit
+        const progreePercent = percent > 100 ? 100 : percent
+
+        const progreeColor = percent < 60
+          ? 'green'
+          : percent < 100
+            ? 'yellow'
+            : 'red'
+      return {
+        ...cat,
+        progreePercent,
+        progreeColor,
+        spend
+      }
+    })
+    this.loading = false
+  },
+}
+</script>
