647f14567aed2f8ed24b95d22bbed8de162b2c5f
Nazar Lesyuk <pronaza@gmail.com>
2020-04-11 14:37:16 +0300

    MainLayout.vue - add error catcher

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -32,6 +32,7 @@
 <script>
 import Navbar from '@/components/app/Navbar';
 import Sidebar from '@/components/app/Sidebar';
+import messages from '@/utils/messages'
 
 export default {
   name: 'main-layout',
@@ -48,6 +49,16 @@ export default {
        await this.$store.dispatch('fetchInfo')
      }
      this.loading = false
+  },
+  computed: {
+    error() {
+      return this.$store.getters.error
+    }
+  },
+  watch: {
+    error(fbError) {
+      this.$error(messages[fbError.code] || 'Что то пошло не так')
+    }
   }
 }
 </script>
