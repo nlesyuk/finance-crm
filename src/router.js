d440d3891c50d48509dd5b8ac7aa7049c7e49265
Nazar Lesyuk <pronaza@gmail.com>
2020-04-11 14:34:06 +0300

    Protect routes

--- src/router.js
+++ src/router.js
@@ -1,19 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from './views/Home.vue';
+import firebase from 'firebase/app'
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'Home',
-      meta: { layout: 'main' },
-      component: Home,
-    },
     {
       path: '/login',
       name: 'login',
@@ -27,40 +22,60 @@ export default new VueRouter({
       component: () => import('@/views/Register.vue'),
     },
     {
+      path: '/',
+      name: 'Home',
+      meta: { layout: 'main', auth: true },
+      component: Home,
+    },
+    {
       path: '/categories',
       name: 'categories',
-      meta: { layout: 'main' },
+      meta: { layout: 'main', auth: true },
       component: () => import('@/views/Categories.vue'),
     },
     {
       path: '/profile',
       name: 'profile',
-      meta: { layout: 'main' },
+      meta: { layout: 'main', auth: true },
       component: () => import('@/views/Profile.vue'),
     },
     {
       path: '/planing',
       name: 'planing',
-      meta: { layout: 'main' },
+      meta: { layout: 'main', auth: true },
       component: () => import('@/views/Planing.vue'),
     },
     {
       path: '/record',
       name: 'record',
-      meta: { layout: 'main' },
+      meta: { layout: 'main', auth: true },
       component: () => import('@/views/Record.vue'),
     },
     {
       path: '/detailrecord',
       name: 'detailrecord',
-      meta: { layout: 'main' },
+      meta: { layout: 'main', auth: true },
       component: () => import('@/views/DetailRecord.vue'),
     },
     {
       path: '/history',
       name: 'history',
-      meta: { layout: 'main' },
+      meta: { layout: 'main', auth: true },
       component: () => import('@/views/History.vue'),
     },
   ],
 });
+
+// protect routes
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser
+  const requireAuth = to.matched.some(record => record.meta.auth)
+
+  if (requireAuth && !currentUser) {
+    next('/login?message=must-login')
+  } else {
+    next()
+  }
+})
+
+export default router
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,6 +1,7 @@
 export default {
   'logout': 'Вы вышли из системы',
   'login': 'Вы ввошли в систему',
+  'must-login': 'Вы не авторизовались',
   'auth/wrong-password': 'Не верный пароль',
   'auth/user-not-found': 'Пользователсь с таким Email не найден',
   'auth/email-already-in-use': 'Email уже занят'
