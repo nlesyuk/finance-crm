f4fb5d911141b6d159c1f5e5d756d4966118bca2
Nazar Lesyuk <pronaza@gmail.com>
2020-04-19 18:49:11 +0300

    Localized Profile - DONE

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -24,13 +24,13 @@
           <ul id='dropdown' class='dropdown-content'>
             <li>
               <router-link to="/profile" class="black-text">
-                <i class="material-icons">account_circle</i>Профиль
+                <i class="material-icons">account_circle</i>{{'ProfileTitle'|localizeFilter}}
               </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
               <a href="#" class="black-text" @click.prevent="logout">
-                <i class="material-icons">assignment_return</i>Выйти
+                <i class="material-icons">assignment_return</i>{{'Exit'|localizeFilter}}
               </a>
             </li>
           </ul>
